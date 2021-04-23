6a706b4d961c6dc7138d0d89d7f39b69bd409b6e
fukui-jin <jinfukui0130@icloud.com>
2021-04-24 03:26:08 +0900

    htmlの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+  has_many :tweets
+  has_many :comments
+  has_many :messages
+  has_many :relationships
+  has_many :room_users
 end
